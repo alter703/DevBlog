8301e7744713f28d0895dc44e62247ee98794395
Anatoliy <anatoliy.brazh130@gmail.com>
2024-02-29 20:48:55 +0200

    Setting up django

--- core/settings.py
+++ core/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default" : {
+    "ENGINE": "django.db.backends.postgresql",
+    "NAME": "your_database_name",
+    "USER": "your_username",
+    "PASSWORD": "your_password",
+    "HOST": "127.0.0.1",
+    "PORT": "5432", # default PostgreSQL port
     }
 }
 
